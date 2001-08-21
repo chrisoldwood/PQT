0cd6e0b9ae3c04822f0d783e7d6868321fb5e8b4
Administrator <administrator@chrisoldwood.com>
2001-08-21 10:10:10 +0000

    Added prefs and find menu items. Added new connection setup dialogs.

--- Resource.h
+++ Resource.h
@@ -14,9 +14,13 @@
 #define ID_QUERY_NEW                    201
 #define ID_QUERY_OPEN                   202
 #define ID_QUERY_SAVEAS                 203
+#define ID_QUERY_PREFS                  204
 #define ID_EXEC_POPUP                   300
 #define ID_EXEC_CURRENT                 301
 #define ID_EXEC_FILE                    302
+#define ID_RESULTS_POPUP                400
+#define ID_RESULTS_FIND                 401
+#define ID_RESULTS_FINDNEXT             402
 #define ID_WINDOW_POPUP                 800
 #define ID_WINDOW_QUERY                 801
 #define ID_WINDOW_RESULTS               802
@@ -25,10 +29,35 @@
 #define IDC_TAB                         1058
 #define IDC_QUERY                       1059
 #define IDC_GRID                        1060
-#define IDC_CONNECTION                  1061
+#define IDC_MINWIDTH                    1062
+#define IDC_LOGIN                       1062
+#define IDC_MAXWIDTH                    1063
+#define IDC_PASSWORD                    1063
+#define IDC_NULLVAL                     1064
+#define IDC_DATABASE                    1064
+#define IDC_ADD                         1065
+#define IDC_EDIT                        1066
+#define IDC_NAME                        1066
+#define IDC_DSN                         1067
+#define IDC_DSNS                        1068
+#define IDC_DRIVER                      1069
+#define IDC_DRIVERS                     1070
+#define IDC_VALUE                       1070
+#define IDC_SERVER                      1071
+#define IDC_FILENAME                    1072
+#define IDC_FILES                       1073
+#define IDC_SQLDIR                      1074
+#define IDC_SQLDIRS                     1075
+#define IDC_COMPUTERS                   1076
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
 #define IDD_CONNECT                     5002
+#define IDD_QUERYPREFS                  5003
+#define IDD_ROWDATA                     5004
+#define IDD_CFGCONNECTION               5005
+#define IDD_DSNS                        5006
+#define IDD_DRIVERS                     5007
+#define IDD_FIND                        5008
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -36,9 +65,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        117
-#define _APS_NEXT_COMMAND_VALUE         129
-#define _APS_NEXT_CONTROL_VALUE         1062
+#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_COMMAND_VALUE         132
+#define _APS_NEXT_CONTROL_VALUE         1071
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
