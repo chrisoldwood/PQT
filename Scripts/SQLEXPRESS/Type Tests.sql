b9365cf37f1d937eeb48d01fbdca110068f8dfe5
Admin <admin@chrisoldwood.com>
2010-10-06 21:27:16 +0000

    Initial version.

---
+++ Scripts/SQLEXPRESS/Type Tests.sql
@@ -0,0 +1,18 @@
+use Test
+
+/*
+
+drop table TypeTest
+
+create table TypeTest
+(
+    Int32Number int not null,
+    Int64Number bigint not null,
+    RealNumber  real not null,
+)
+
+insert into TypeTest values(2147483647, 9876543210, 9876543210)
+
+*/
+select * from TypeTest
+
