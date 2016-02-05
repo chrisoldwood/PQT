7aa1c9da06f935c2dc322bdee41a4284f3cfa2d0
Chris Oldwood <gort@cix.co.uk>
2016-02-05 17:12:57 +0000

    Added datetime examples to the type-tests script.

--- Scripts/SQLEXPRESS/Type Tests.sql
+++ Scripts/SQLEXPRESS/Type Tests.sql
@@ -1,7 +1,9 @@
 select
-    cast(1 as bit)          as [bit],
-    cast(1 as tinyint)      as [tinyint],
-    cast(1 as smallint)     as [smallint],
-    cast(1 as int)          as [int],
-    cast(1 as bigint)       as [bigint],
-    cast(1 as decimal(2,1)) as [decimal]
+      cast(1 as bit)          as [bit]
+    , cast(1 as tinyint)      as [tinyint]
+    , cast(1 as smallint)     as [smallint]
+    , cast(1 as int)          as [int]
+    , cast(1 as bigint)       as [bigint]
+    , cast(1.5 as decimal(2,1)) as [decimal]
+    , cast('2006-05-04T01:02:03.555' as smalldatetime) as [smalldatetime]
+    , cast('2006-05-04T01:02:03.555' as datetime)      as [datetime]
